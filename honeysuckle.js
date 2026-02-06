68824332b947fe2d9353e297c2bbdb9a9369e9ad
npope1234 <97932490+npope1234@users.noreply.github.com>
2026-02-06 01:10:46 -0500

    Add files via upload

---
+++ honeysuckle.js
@@ -0,0 +1,12 @@
+var invasive = {
+    "type": "Feature",
+    "properties": {
+        "name": "Kentucky",
+        "amenity": "Baseball Stadium",
+        "popupContent": "Japanese Honesuckle in Kentucky"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [38.25333, -82.98500]
+    }
+};
